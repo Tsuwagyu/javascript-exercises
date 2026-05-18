7ecc0a55c5c5c9aca16a3a08875e469fc1c4a7f9
mercury-Hg80 <150161680+mercury-Hg80@users.noreply.github.com>
2026-05-18 15:49:13 -0400

    complete function for extracting info from tree using helper func

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,26 @@
-const contains = function() {
-  
+const contains = function(obj, value) {
+
+
+    function isEqual(currentThingFromObj, valueWeAreLookingFor) {
+
+        if (Number.isNaN(currentThingFromObj) && Number.isNaN(valueWeAreLookingFor)) return true;
+
+        return currentThingFromObj === valueWeAreLookingFor;
+
+    }
+    // base case, if we find the thing we need immediately return true 
+    for (const current of Object.values(obj)) {
+        if (isEqual(current, value)) return true;
+    
+        // to check if u can search inside the object, if u can then use recursion
+        if (typeof current === "object" && current !== null) {
+            if(contains(current, value)) return true;
+        }
+    // base case 2 means we went over the object and found nothing so return false
+    }
+    return false
+
+
 };
   
 // Do not edit below this line
