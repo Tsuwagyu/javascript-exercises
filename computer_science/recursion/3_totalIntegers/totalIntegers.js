15a823ccc4d0611a051395365166b736536b08fa
mercury-Hg80 <150161680+mercury-Hg80@users.noreply.github.com>
2026-05-18 17:28:16 -0400

    implement recursive totalIntegers function

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,4 +1,28 @@
-const totalIntegers = function() {
+const totalIntegers = function(item, value) {
+
+    // reject non objects, and any values that are null, nan, or a string
+
+    if (typeof item !== "object") {
+        return undefined;
+    }
+
+    // recursive case
+
+    let count = 0;
+
+    for (const value of Object.values(item)) {
+        if (Number.isInteger(value)) {
+            count++; 
+        }
+
+        if (typeof value === "object" && value !== null) {
+            count+= totalIntegers(value) ;
+        }
+    }
+
+    return count;
+
+
   
 };
   
