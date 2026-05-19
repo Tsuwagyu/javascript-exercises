848df756ff9f0c3ca557a04654d63a1186a72294
mercury-Hg80 <150161680+mercury-Hg80@users.noreply.github.com>
2026-05-19 12:42:55 -0400

    add permutations function

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,36 @@
-const permutations = function() {
-  
+const permutations = function(arr) {
+
+    console.log("permutations called with:", arr);
+
+    const results = [];
+
+    // base cases
+    if (arr.length === 0) return [[]];
+
+    // recursive case
+
+
+    const remainingArr = arr.slice(1);
+    const first = arr[0];
+    const subPermutations = permutations(remainingArr);
+
+    for (const sub of subPermutations) {
+        results.push([first, ...sub]);
+    }
+    return results;
+
+
+    
+    
+
+
+
+
+
 };
+
+console.log(permutations([1, 2, 3]));
+
   
 // Do not edit below this line
 module.exports = permutations;
