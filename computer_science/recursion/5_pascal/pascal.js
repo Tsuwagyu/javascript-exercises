1504ee8fd6bd09124837ba1eed5d59d2ac2e0c9c
mercury-Hg80 <150161680+mercury-Hg80@users.noreply.github.com>
2026-05-19 15:37:35 -0400

    complete pascal function

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,4 +1,22 @@
-const pascal = function() {
+const pascal = function(n) {
+
+    if (n === 1) return [1];
+
+    let previousRow = pascal(n-1);
+    const newRow = [1];
+
+
+    for (let i = 0; i < previousRow.length - 1; i++) {
+
+        newRow.push(previousRow[i] + previousRow[i+1]);
+
+    }
+
+    newRow.push(1);
+
+    return newRow;
+
+    
   
 };
   
